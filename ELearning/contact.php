4ad2954c0638ce8a70a9c57254e0c08ea80848fd
Rama Krishna Parjanya Varanasi <65504620+vrkparjanya@users.noreply.github.com>
2020-11-23 23:14:48 +0530

    Add files via upload

---
+++ ELearning/contact.php
@@ -0,0 +1,18 @@
+<!--Start Contact Us-->
+<div class="container mt-4" id="Contact" >  <!--Start Contact Us Container-->
+      <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+      <div class="row">  <!--Start Contact Us Row-->
+        <div class="col-md-8"> <!--Start Contact Us 1st Column-->
+          <form action="" method="post">
+            <input type="text" class="form-control" name="name" placeholder="Name"><br>
+            <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+            <input type="email" class="form-control" name="email" placeholder="E-mail"><br>
+            <textarea class="form-control" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br>
+            <input class="btn btn-primary" type="submit" value="Send" name="submit"><br><br>
+          </form>
+        </div> <!-- End Contact Us 1st Column-->
+
+         <!-- End Contact Us 2nd Column-->
+      </div> <!-- End Contact Us Row-->
+    </div> <!-- End Contact Us Container-->
+    <!-- End Contact Us -->
